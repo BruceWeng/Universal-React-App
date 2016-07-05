ee6d4223c5fa199e25761bf64da6863699c34dfe
BruceWeng <weng8916@gmail.com>
2016-07-05 14:20:39 -0700

    use webpack to build a client-side bundle that will mount component to the DOM

--- Component.jsx
+++ Component.jsx
@@ -1,17 +1,26 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  _handleClick: function() {
-    alert();
+  _handleClick: function () {
+    alert('Succeed in client-side rendering!');
   },
 
   render: function () {
     return (
-      <div>
-        <h1>Hello World!</h1>
-        <p>Isn't server-side rendering remarkable?</p>
-        <button onClick={this._handleCLick}>Click Me</button>
-      </div>
+      <html>
+        <head>
+          <title>Universal App with React</title>
+          <link rel='stylesheet' href='/style.css' />
+        </head>
+        <body>
+          <div>
+            <h1>Hello World!</h1>
+            <p>Isn't server-side rendering remarkable?</p>
+            <button onClick={this._handleClick}>Click Me</button>
+          </div>
+          <script src='/bundle.js' />
+        </body>
+      </html>
     );
   }
 });
