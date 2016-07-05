f0578ee313bd1ba3e9acb39e787d9dd529254cb4
BruceWeng <weng8916@gmail.com>
2016-07-05 12:52:49 -0700

    Server-side rendering with react-dom renderToString

---
+++ Component.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  _handleClick: function() {
+    alert();
+  },
+
+  render: function () {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <p>Isn't server-side rendering remarkable?</p>
+        <button onClick={this._handleCLick}>Click Me</button>
+      </div>
+    );
+  }
+});
